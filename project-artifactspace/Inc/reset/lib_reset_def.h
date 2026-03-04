5d55cc8897c30b4a31d801b8a640d1b561fbcc03
Huỳnh Thanh Sang <macsin233@gmail.com>
2026-03-04 11:32:22 +0700

    refactor(reset): improve reset handling with bit manipulation and enhance register definitions

--- project-artifactspace/Inc/reset/lib_reset_def.h
+++ project-artifactspace/Inc/reset/lib_reset_def.h
@@ -26,27 +26,27 @@
 
     #define SCB_AIRCR_REG_ADDR (SCB_REGS_BASEADDR + 0x0Cul)
 
-  // Khai báo cấu trúc thanh ghi
-
-    tdf_strc SCB_AIRCR_REG
-    {
-      __vo unsigned int VECTRESET : 1;
-      __vo unsigned int VECTCLRACTIVE : 1;
-      __vo unsigned int SYSRESETREQ : 1;
-      __vo unsigned int RESERVED0 : 5;
-      __vo unsigned int PRIGROUP : 3;
-      __vo unsigned int RESERVED1 : 4;
-      __vo unsigned int ENDIANNESS : 1;
-      __vo unsigned int VECTKEY : 16;
-    } SCB_AIRCR_REG;
-
   // >> Tạo con trỏ tới thanh ghi
 
     #ifndef UNIT_TEST
-      #define SCB_AIRCR_REG_PTR ((SCB_AIRCR_REG *) SCB_AIRCR_REG_ADDR)
+      /**
+       * Ghi chú:
+       * Sử dụng khai báo biến, 
+       * không sử dụng define
+       */
+
+      __vo BLANK_REG* SCB_AIRCR_REG_PTR = (__vo BLANK_REG *)SCB_AIRCR_REG_ADDR;
+      
     #else
-      extern SCB_AIRCR_REG MOCK_SCB_AIRCR_REG;
-      #define SCB_AIRCR_REG_PTR (&MOCK_SCB_AIRCR_REG)
+      /**
+       * Ghi chú:
+       * Trong môi trường unit test, không thể khai báo biến con trỏ tới thanh ghi thực tế,
+       * do cần sử dụng biến giả để mô phỏng giá trị thanh ghi và kiểm tra trong các test case.
+       */  
+
+      uint32_t mock_aircr_reg = 0; 
+      __vo BLANK_REG* SCB_AIRCR_REG_PTR = &mock_aircr_reg; // Trỏ vào biến giả lập
+      
     #endif
 
     /**
@@ -56,32 +56,20 @@
 
   // Khai báo các định nghĩa bit cần sử dụng trên RCC_CSR_REG
 
-    #define RCC_CSR_REG_RMVF_NO_EFFECT RESET
-		#define RCC_CSR_REG_RMVF_CLEAR SET
-
-		#define RCC_CSR_REG_PINRSTF_NO_RESET_OCCURRED RESET
-		#define RCC_CSR_REG_PINRSTF_RESET_OCCURRED SET
-
-		#define RCC_CSR_REG_PORRSTF_NO_RESET_OCCURRED RESET
-		#define RCC_CSR_REG_PORRSTF_RESET_OCCURRED SET
-
-		#define RCC_CSR_REG_SFTRSTF_NO_RESET_OCCURRED RESET
-		#define RCC_CSR_REG_SFTRSTF_RESET_OCCURRED SET
-
-		#define RCC_CSR_REG_IWDGRSTF_NO_RESET_OCCURRED RESET
-		#define RCC_CSR_REG_IWDGRSTF_RESET_OCCURRED SET
-
-		#define RCC_CSR_REG_WWDGRSTF_NO_RESET_OCCURRED RESET
-		#define RCC_CSR_REG_WWDGRSTF_RESET_OCCURRED SET
-
-		#define RCC_CSR_REG_LPWRRSTF_NO_RESET_OCCURRED RESET
-		#define RCC_CSR_REG_LPWRRSTF_RESET_OCCURRED SET
+    #define RCC_CSR_REG_MASK ((ui32)0xFFFFFFFFul)
+    #define RCC_CSR_REG_PINRSTF_OCCURRED  ((ui32)1u << 26)
+    #define RCC_CSR_REG_PORRSTF_OCCURRED  ((ui32)1u << 27)
+    #define RCC_CSR_REG_SFTRSTF_OCCURRED  ((ui32)1u << 28)
+    #define RCC_CSR_REG_IWDGRSTF_OCCURRED ((ui32)1u << 29)
+    #define RCC_CSR_REG_WWDGRSTF_OCCURRED ((ui32)1u << 30)
+    #define RCC_CSR_REG_LPWRRSTF_OCCURRED ((ui32)1u << 31)
+    #define RCC_CSR_REG_RMVF_SET ((ui32)1u << 24)
 
   // Khai báo các định nghĩa bit cần sử dụng trên SCB_AIRCR_REG
 
-    #define SCB_AIRCR_REG_VECTKEY_RESET 0xFA05
-    #define SCB_AIRCR_REG_VECTKEY_SET 0x05FA
-    #define SCB_AIRCR_REG_SYSRESETREQ_RESET RESET
-    #define SCB_AIRCR_REG_SYSRESETREQ_SET SET
+    #define SCB_AIRCR_REG_VECTKEY_RESET ((ui32)0xFA05 << 16)
+    #define SCB_AIRCR_REG_VECTKEY_SET   ((ui32)0x05FA << 16)
+    #define SCB_AIRCR_REG_SYSRESETREQ_SET ((ui32)1u << 2)
+    #define SCB_AIRCR_REG_SYSRESETREQ_CLEAR ~((ui32)1u << 2)
 
 #endif /* LIB_RESET_DEF_H_ */
